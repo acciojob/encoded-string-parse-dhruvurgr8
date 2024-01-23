347177b243dc88b4c2393f2fcde377666f60153e
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-23 20:08:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const parseCode = (str) => {
   const[firstName,lastName,id] = str.split('000');
 	return {
-		firstName:firstName,
-		lastName:lastName,
-		id:id
+		"firstName":firstName,
+		"lastName":lastName,
+		"id":id
 	}
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
