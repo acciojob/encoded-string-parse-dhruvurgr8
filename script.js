d674c4cd9fc437f85e42f8a7973ba0b924ecbe54
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-23 19:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const parseCode = (str) => {
-  // your code here
+  const[firstName,lastName,id] = str.split('000');
+	return {
+		firstName:firstName,
+		lastName:lastName,
+		id:id
+	}
 };
 
 // Do not change the code below
